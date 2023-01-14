296a213e21828a4c2e4a62e4b11f40996f28dea7
Denys Maiolli <denysmaiolli@hotmail.com>
2023-01-13 21:51:04 -0300

    feat: Adding send message to topic

---
+++ src/main/java/com/maiolly/denys/pockafka/core/domain/dto/output/MasterBlasterEventPayload.java
@@ -0,0 +1,4 @@
+package com.maiolly.denys.pockafka.core.domain.dto.output;
+
+public record MasterBlasterEventPayload(String processId, String message) {
+}
---
+++ src/main/java/com/maiolly/denys/pockafka/dataprovider/broker/masterblaster/event/MasterBlasterEvent.java
@@ -0,0 +1,25 @@
+package com.maiolly.denys.pockafka.dataprovider.broker.masterblaster.event;
+
+import com.maiolly.denys.pockafka.core.domain.dto.output.MasterBlasterEventPayload;
+import com.maiolly.denys.pockafka.dataprovider.broker.common.event.BrokerEvent;
+
+public record MasterBlasterEvent(MasterBlasterEventPayload payload) implements BrokerEvent<MasterBlasterEventPayload> {
+
+    private static final String MASTER_BLASTER_EVENT = "masterBlasterEvent";
+
+    @Override
+    public String name() {
+        return MASTER_BLASTER_EVENT;
+    }
+
+    @Override
+    public String applicationName() {
+        return "ms-master-blaster-application";
+    }
+
+    @Override
+    public String eventId() {
+        return payload.processId();
+    }
+
+}
