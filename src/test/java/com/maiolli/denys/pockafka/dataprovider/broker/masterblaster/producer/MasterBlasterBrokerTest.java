07dcc81591c43b33a4cee96f03b7028e8d0de93a
Denys Maiolli <denysmaiolli@hotmail.com>
2023-01-20 21:45:04 -0300

    refact: Removing imports

--- src/test/java/com/maiolli/denys/pockafka/dataprovider/broker/masterblaster/producer/MasterBlasterBrokerTest.java
+++ src/test/java/com/maiolli/denys/pockafka/dataprovider/broker/masterblaster/producer/MasterBlasterBrokerTest.java
@@ -3,19 +3,12 @@ package com.maiolli.denys.pockafka.dataprovider.broker.masterblaster.producer;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.maiolli.denys.pockafka.ContextTests;
 import com.maiolli.denys.pockafka.core.domain.dto.output.MasterBlasterEventPayload;
-import com.maiolli.denys.pockafka.entrypoint.consumer.MasterBlasterConsumerEvent;
 import org.awaitility.Awaitility;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.kafka.test.EmbeddedKafkaBroker;
-import org.springframework.messaging.Message;
 
 import java.util.concurrent.TimeUnit;
-
-import static org.mockito.Mockito.*;
 
 class MasterBlasterBrokerTest extends ContextTests {
 
