289cd9322a974621dbd0988662beb20855004c6c
Denys Maiolli <denysmaiolli@hotmail.com>
2023-01-20 21:25:26 -0300

    feat: Adicionando save no usecase

--- src/main/java/com/maiolli/denys/pockafka/core/gateway/MasterBlasterGateway.java
+++ src/main/java/com/maiolli/denys/pockafka/core/gateway/MasterBlasterGateway.java
@@ -2,18 +2,27 @@ package com.maiolli.denys.pockafka.core.gateway;
 
 import com.maiolli.denys.pockafka.core.domain.dto.output.MasterBlasterEventPayload;
 import com.maiolli.denys.pockafka.dataprovider.broker.masterblaster.producer.MasterBlasterBroker;
+import com.maiolli.denys.pockafka.dataprovider.database.entity.MasterBlasterEntity;
+import com.maiolli.denys.pockafka.dataprovider.database.repository.MasterBlasterRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface MasterBlasterGateway {
     void sendToQueue(MasterBlasterEventPayload payload);
+
+    MasterBlasterEntity save(MasterBlasterEntity entity);
 }
 
 @Service
-record MasterBlasterGatewayImpl(MasterBlasterBroker blasterBroker) implements MasterBlasterGateway {
+record MasterBlasterGatewayImpl(MasterBlasterBroker blasterBroker, MasterBlasterRepository repository) implements MasterBlasterGateway {
 
     @Override
     public void sendToQueue(MasterBlasterEventPayload payload) {
         blasterBroker.send(payload);
+    }
+
+    @Override
+    public MasterBlasterEntity save(MasterBlasterEntity entity) {
+        return repository.save(entity);
     }
 }
--- src/main/java/com/maiolli/denys/pockafka/core/usecase/ReceiveMasterBlasterUseCase.java
+++ src/main/java/com/maiolli/denys/pockafka/core/usecase/ReceiveMasterBlasterUseCase.java
@@ -1,14 +1,15 @@
 package com.maiolli.denys.pockafka.core.usecase;
 
 import com.maiolli.denys.pockafka.core.domain.dto.output.MasterBlasterEventPayload;
-import com.maiolli.denys.pockafka.core.exception.SendKafkaMessageException;
+import com.maiolli.denys.pockafka.core.gateway.MasterBlasterGateway;
+import com.maiolli.denys.pockafka.dataprovider.database.entity.MasterBlasterEntity;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ReceiveMasterBlasterUseCase {
+public record ReceiveMasterBlasterUseCase(MasterBlasterGateway gateway) {
 
     public void process(MasterBlasterEventPayload payload) {
-        throw new SendKafkaMessageException(new RuntimeException());
+        gateway.save(new MasterBlasterEntity(payload.processId(), payload.message()));
     }
 
 }
